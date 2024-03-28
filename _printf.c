7c015ef4f4b9044898b7bfb5726957edd0adc95c
Thomas3300000 <8844@holbertonstudents.com>
2024-03-28 12:38:50 +0000

    addind a check to display ! and K after a %

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (cptform = 0; format[cptform] != '\0'; cptform++)
 	{
-		if (format[cptform] == '%')
+		if (format[cptform] == '%' && format[cptform + 1] != '!' && format[cptform + 1] != 'K')
 		{
 			cptform++;
 			for (cptype = 0; types[cptype].type != '\0'; cptype++)
